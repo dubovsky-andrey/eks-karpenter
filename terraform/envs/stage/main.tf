2af8a1a2ff7142b0f5cde5db08b303d66b7c0bcc
andrey dubovsky <dubovsky.andrey.work@gmail.com>
2025-08-05 11:28:46 -0400

    change strukture

--- terraform/envs/stage/main.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_s3_bucket" "state" {
-  bucket        = "${var.project_name}-state-karpenter-${var.region}-${var.aws_account_id}"
-  force_destroy = true
-}
--- terraform/envs/stage/providers.tf
+++
@@ -1,5 +0,0 @@
-
-provider "aws" {
-  region = var.region
-  allowed_account_ids = [var.aws_account_id]
-}
